c9df795a39feefd22d8b9a628964dcbea80af252
Cube <116672365+ZLCube@users.noreply.github.com>
2022-12-25 04:13:58 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -124,14 +124,21 @@ sudo cp -R Everblush /usr/share/themes/
 cd ~/.local/share/icons
 sudo cp -Rv Nordzy-cyan-dark-MOD /usr/share/icons
 
+# Creando carpeta de Reposistorios
+
+cd $ruta
+mkdir $ruta/github
+
 # Instalando Picom
 
 sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
+cd ~/github/picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
 sudo cp -r $ruta/Components/picom-config/picom.conf ~/.config/
 
-# Creando carpeta de Reposistorios
-cd $ruta
-mkdir $ruta/github
 
 # Descargar Repositorios Necesarios
 
