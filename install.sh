ee469652dedddfc6e486449f85e0285103c13e63
Cube <116672365+ZLCube@users.noreply.github.com>
2022-12-25 05:17:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -132,7 +132,10 @@ mkdir $ruta/github
 # Instalando Picom
 
 sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
-cd ~/github/picom
+
+cd $ruta/github
+git clone https://github.com/ibhagwan/picom.git
+cd $ruta/github/picom
 git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
